ce17b4731ca321e112022a472bf94b302c7d144e
Andrew Knowles <87070974+AndrewJKnowles@users.noreply.github.com>
2022-07-25 17:04:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 /*  PWM Example
+*   ===========
+*
+*   Function:
 *   This example shows two ways to control the PWM output to an LED. The first uses a pot to set the PWM
 *   duty cycle, while the second cycles from 0% (Off) up to 100% duty cycle and then back to 0%. The first
 *   example is commented out (Line 23 to 27) while the second example is define in lines 29 to 46.
+*  
+*   Circuit Schematic No.:  
+*   Required Libraries:     
 *
-*   Author: Andrew Knowles 2021
-*/
+*   Authored by:            Andrew Knowles
+*   Date:                   2021
+*   Version:                1.0
+*   Revision Date:          
+*   MBED Studio Version:    1.4.1
+*   MBED OS Version:        6.14.0
+*   Board:	                NUCLEO L476RG*/
 
 #include "mbed.h"
 
